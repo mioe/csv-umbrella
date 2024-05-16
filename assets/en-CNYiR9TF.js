7d4063696247097e1bf15593abc236a1b6ff69b0
mioe <mioe.boss@gmail.com>
2024-05-16 07:52:08 +0000

    Deploying to gh-pages from @ mioe/csv-umbrella@ffab541697b1b2cf4e82cc23edf4b50d853d75cb 🚀

--- assets/en-CNYiR9TF.js
+++
@@ -1,1 +0,0 @@
-const e={hello:{t:0,b:{t:2,i:[{t:3}],s:"hello"}},"choose-file":{t:0,b:{t:2,i:[{t:3}],s:"Choose file"}},reset:{t:0,b:{t:2,i:[{t:3}],s:"Reset"}}};export{e as default};
---
+++ assets/en-CdPOdYxb.js
@@ -0,0 +1,1 @@
+const t={hello:{t:0,b:{t:2,i:[{t:3}],s:"hello"}},"choose-file":{t:0,b:{t:2,i:[{t:3}],s:"Choose file"}},reset:{t:0,b:{t:2,i:[{t:3}],s:"Reset"}},about:{t:0,b:{t:2,i:[{t:3}],s:"Experimental project for uploading and formatting CSV files"}},"sys-field":{t:0,b:{t:2,i:[{t:3}],s:"System columns"}},"custom-field":{t:0,b:{t:2,i:[{t:3}],s:"User columns"}}};export{t as default};
